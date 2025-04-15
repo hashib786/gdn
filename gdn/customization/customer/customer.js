5bd2806280ba99c147d061cad2f3ec4517b9d535
Dhairya <dhairya@extensioncrm.com>
2025-04-15 06:58:12 +0000

    feat: implement custom logs and hide fields in customer form

--- gdn/customization/customer/customer.js
+++ gdn/customization/customer/customer.js
@@ -1,3 +1,39 @@
+const defaultFillTheVal = [{
+    field: "custom_agreement_verification_status",
+    fieldName: "Agreement Verification Status",
+    value: "custom_old_agreement_verified_value",
+    dateTime: "custom_agreement_changed_date_time"
+},
+{
+    field: "custom_onboarding_status",
+    fieldName: "Onboarding Status",
+    value: "custom_old_onboarding_status_verified",
+    dateTime: "custom_onboarding_status_change_date_time"
+},
+{
+    field: "custom_onboarding_date",
+    fieldName: "Onboarding Date",
+    value: "custom_old_onboarding_date_value",
+    dateTime: "custom_onboarding_change_date_time"
+},
+{
+    field: "custom_kyc_verification_status",
+    fieldName: "KYC Verification Status",
+    value: "custom_old_kyc_value_",
+    dateTime: "custom_kyc_change_date_and_time"
+}]
+
+const dateTimeFields = ["custom_agreement_changed_date_time"];
+
+const hideFields = ["custom_old_agreement_verified_value",
+    "custom_agreement_changed_date_time",
+    "custom_old_onboarding_status_verified",
+    "custom_onboarding_status_change_date_time",
+    "custom_old_onboarding_date_value",
+    "custom_onboarding_change_date_time",
+    "custom_old_kyc_value_",
+    "custom_kyc_change_date_and_time"];
+
 frappe.ui.form.on("Customer", {
     custom_status: function (frm) {
         if (frm.doc.custom_status == "Inactive") {
@@ -28,9 +64,37 @@ frappe.ui.form.on("Customer", {
     },
     custom_onboarding_status: function (frm) {
         frm.set_value("custom_onboarding_status", Boolean(frm.doc.custom_agreement_doc && frm.doc.custom_kyc_doc) ? "Completed" : "Pending");
-        if(frm.doc.custom_onboarding_status == "Completed") {
+        if (frm.doc.custom_onboarding_status == "Completed") {
             frm.set_value("custom_onboarding_date", frappe.datetime.now_date());
         }
+    },
+    custom_agreement_approved: function (frm) {
+    },
+    before_save: function (frm) {
+        defaultFillTheVal.forEach((field) => {
+            if ((!frm.doc[field.value] && frm.doc[field.field]) || (frm.doc[field.value] !== frm.doc[field.field])) {
+                const currentDateTime = frappe.datetime.now_datetime();
+                const newValue = [{
+                    "custom_field_name": field.fieldName,
+                    "from": frm.doc[field.value] || "Blank",
+                    "to": frm.doc[field.field],
+                    "custom_field_api_name": field.field,
+                    "from_date": frm.doc[field.dateTime] || currentDateTime,
+                    "to_date": currentDateTime,
+                    custom_changed_by: frm.doc.modified_by,
+                    custom_changed_durations: formatDurationFromMinutesWithSeconds(
+                        frappe.datetime.get_minute_diff(
+                            currentDateTime,
+                            frm.doc[field.dateTime] || currentDateTime,
+                        )
+                    ),
+                }, ...frm.doc.custom_logs || []];
+                console.log({ newValue })
+                frm.set_value("custom_logs", newValue);
+                frm.set_value(field.dateTime, frappe.datetime.now_datetime());
+                frm.set_value(field.value, frm.doc[field.field]);
+            }
+        });
     },
     onload: function (frm) {
         // Read Only custom_agreement_approved, custom_kyc_
@@ -39,6 +103,9 @@ frappe.ui.form.on("Customer", {
             "custom_kyc_",
         ];
         readOnlyFields.forEach((field) => frm.set_df_property(field, "read_only", 1));
+
+        // Hide fields
+        hideFields.forEach((field) => frm.set_df_property(field, "hidden", 1));
     },
     refresh: function (frm) {
         // frm.add_custom_button(__('Validate Website'), function() {
@@ -58,7 +125,8 @@ frappe.ui.form.on("Customer", {
         frm.remove_custom_button("Validate Website");
         frm.remove_custom_button("Accounts Receivable", "View");
         frm.remove_custom_button("Accounting Ledger", "View");
-    }
+    },
+
 });
 
 function validateWebsite(frm) {
@@ -108,3 +176,24 @@ function validateWebsite(frm) {
     return true;
 }
 
+
+function formatDurationFromMinutesWithSeconds(minutesStr) {
+    // Convert to BigInt for large safe numbers
+    console.log({ minutesStr });
+    const totalSeconds = BigInt(minutesStr) * 60n;
+
+    const secondsInDay = 24n * 60n * 60n;
+    const secondsInHour = 60n * 60n;
+    const secondsInMinute = 60n;
+
+    const days = totalSeconds / secondsInDay;
+    const remainingAfterDays = totalSeconds % secondsInDay;
+
+    const hours = remainingAfterDays / secondsInHour;
+    const remainingAfterHours = remainingAfterDays % secondsInHour;
+
+    const minutes = remainingAfterHours / secondsInMinute;
+    const seconds = remainingAfterHours % secondsInMinute;
+
+    return `${days} days, ${hours} hours, ${minutes} minutes`;
+}
