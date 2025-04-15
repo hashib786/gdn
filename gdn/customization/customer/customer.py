5dac4f6e0d9eafd0b647668f67c3e1190fdaf81b
Dhairya <dhairya@extensioncrm.com>
2025-04-15 06:58:23 +0000

    feat: add initial customer.py file with duration formatting function

---
+++ gdn/customization/customer/customer.py
@@ -0,0 +1,11 @@
+# from frappe.utils import format_duration
+# import frappe
+
+# @frappe.whitelist()
+# def get_format_duration(minutes):
+#     minutes = int(minutes)
+    
+#     if minutes > 100000:
+#         return "Duration too large to format"
+    
+#     return format_duration(minutes)
