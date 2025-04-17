994571e460e3c412ec98a832eace89963d221fd2
Dhairya <dhairya@extensioncrm.com>
2025-04-17 11:51:36 +0000

    feat : added funtion with one checkbox show the custom_grant_proposal_document field

--- gdn/gdn/doctype/grant/grant.js
+++ gdn/gdn/doctype/grant/grant.js
@@ -1,8 +1,11 @@
 // Copyright (c) 2024, extension and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Grant", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Grant", {
+    refresh(frm) {
+        frm.toggle_display("custom_grant_proposal_document", frm.doc.custom_documents_required);
+    },
+    custom_documents_required(frm) {
+        frm.toggle_display("custom_grant_proposal_document", frm.doc.custom_documents_required);
+    }
+});
